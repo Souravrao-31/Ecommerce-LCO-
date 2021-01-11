80edb0c84f72eeb8367931b2531c7ae438b4812f
Sourav <souravrao3110@gmail.com>
2021-01-11 20:57:22 +0530

    added middleware and establish database connection

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser')  //for middleware
+const cookieParser = require('cookie-parser')  //for middleware
+const cors = require('cors')       //for middleware
+const router = require('router')
 
+
+
+//DB Connections
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -13,8 +20,20 @@ mongoose.connect(process.env.DATABASE,{
     console.log("SOMETHING WRONG WITH.... DATABASES");
 })
 
+
+//Middlewares
+app.use(bodyParser.json);
+app.use(cookieParser());
+app.use(cors());
+
+
+//My Routes
+app.use("/api", authRoutes);
+
+  //PORt
  const port = process.env.PORT || 8000;
 
+ //Starting Server
  app.listen(port, ()=> {
      console.log(`App is running at ${port}`)
  });
