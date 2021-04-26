fe2650aa5917b5e0ff6700a56bda02481ba37610
Sourav <souravrao3110@gmail.com>
2021-04-26 15:56:59 +0530

    Updated UUid

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 const crypto = require('crypto')
-const uuidv1 = require('uuid/v1');
+﻿const { v4: uuidv4 } = require('uuid');
 
 var userSchema = new mongoose.Schema({
     name : {
@@ -46,23 +46,24 @@ var userSchema = new mongoose.Schema({
 userSchema.virtual("password")
       .set(function(password) {
         this._password = password
-        this.salt = uuidv1()
+        this.salt = uuidv4()
         this.encrypT_password = this.securePassword(password);
       })
       .get(function(){
         return this._password
       })
 
-userSchema.method = {
+userSchema.methods = {
 
   authenticate: function(plainpassword){
     return this.securePassword(plainpassword === this.encrypT_password)
   },
 
   securePassword: function(plainpassword){
-    if(!password) return "";
+    if(!plainpassword) return "";
     try{
-      return crypto.createHmac('sha256', this.salt)
+      return crypto
+      .createHmac('sha256', this.salt)
       .update(plainpassword)
       .digest('hex')
     }catch (err){
@@ -72,4 +73,4 @@ userSchema.method = {
   }
 }
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
