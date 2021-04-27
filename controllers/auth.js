4097b9ea05fd25b1f11e78e05f4d30ad6f1b1d73
Sourav <souravrao3110@gmail.com>
2021-04-27 19:15:05 +0530

    Save data in DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,34 @@
 //going to routes 
+const User = require('../models/user')
 
-exports.signup = (req, res) =>{
-    res.json({
-        message: "user signin"
-    });
+
+exports.signup = (req, res) => {
+   
+  
+//    console.log("REQ BODY", req.body)
+//     res.json({
+//         message: " sign in...PP"
+//     });
+
+ const user = new User(req.body) //saving in database
+   user.save( (err, user) => {
+       if(err){
+           return res.status(400).json({
+             err: "NOT able to save DB"
+           })
+       }
+       res.json({
+           name: user.name,
+           email: user.email,
+           id: user._id
+       });
+   })
+   
 };
 
 exports.signout = (req, res) =>{
     res.json({
-        message: "user signout"
+        message: "user signout..."
     });
 };
 
