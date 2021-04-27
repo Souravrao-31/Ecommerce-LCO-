5e8b22441cd193836d81a94162d222a927c36227
Sourav <souravrao3110@gmail.com>
2021-04-27 19:30:46 +0530

    Validator

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const {check}   = require('express-validator');
 
 router.post("/signup", [
     check("name", "Should be atleast 3 character ").isLength({ min: 3 }),
-    check("email", "Email is Required ").isEmail(),
+    check("email", "Email is not valid ").isEmail(),
     check("password", "Password is Required ").isLength({ min: 4})
 ], signup)  //from controller
 router.get("/signout", signout) //routes on controllers
