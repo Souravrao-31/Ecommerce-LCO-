7857af1f07d9275b480c979cd04fb3b9e741dd4d
Sourav <souravrao3110@gmail.com>
2021-04-27 19:16:06 +0530

    Added Data Validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,15 @@
 var express = require('express')
 var router = express.Router()
 
-const {signout} = require('../controllers/auth')
-const {signup} = require('../controllers/auth')
+const {signout, signup} = require('../controllers/auth')
 
-router.post("/signup", signup)  //from controller
+const {check}   = require('express-validator');
+
+router.post("/signup", [
+    check("name", "Should be atleast 3 character ").isLength({ min: 3 }),
+    check("email", "Email is Required ").isEmail(),
+    check("password", "Password is Required ").isLength({ min: 4})
+], signup)  //from controller
 router.get("/signout", signout) //routes on controllers
 
 module.exports = router;
